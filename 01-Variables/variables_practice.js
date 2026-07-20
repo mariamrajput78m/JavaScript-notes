89f18afb7d7db3b9ec4fa0e1199c63c1cb25db15
Mariam's Github <228037552+mariamrajput78m@users.noreply.github.com>
2026-07-20 20:18:59 +0000

    practice question

---
+++ 01-Variables/variables_practice.js
@@ -0,0 +1,18 @@
+/*
+Write code that:
+Stores your name.
+Stores your age.
+Stores your city.
+Stores your semester.
+Stores your favorite color.
+Prints all of them using console.log().
+ */
+
+let name = "Mariam";
+let age = 19;
+let city = "Lahore";
+let sem = "5th";
+let color = "White";
+
+console.log(name , age , city , sem , color);
+
