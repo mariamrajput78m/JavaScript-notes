45503313f62c38636a09291b724e83ce684201a8
Mariam's Github <228037552+mariamrajput78m@users.noreply.github.com>
2026-07-28 18:03:16 +0000

    changing style

---
+++ 12-DOM/03-changing-style.js
@@ -0,0 +1,6 @@
+const heading = document.getElementById("title");
+
+heading.style.color = "red";
+heading.style.backgroundColor = "yellow";
+heading.style.padding = "10px";
+heading.style.borderRadius = "8px";
