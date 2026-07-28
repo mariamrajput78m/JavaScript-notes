d11d90299dd5c6130a0fe6fc85ea7c87d5daaed7
Mariam's Github <228037552+mariamrajput78m@users.noreply.github.com>
2026-07-28 18:23:56 +0000

    DOM manipulatin

---
+++ 12-DOM/07-tranversing-DOM.js
@@ -0,0 +1,13 @@
+const list = document.getElementById("list");
+
+console.log(list.parentElement);
+
+console.log(list.children);
+
+console.log(list.firstElementChild);
+
+console.log(list.lastElementChild);
+
+console.log(list.nextElementSibling);
+
+console.log(list.previousElementSibling);
