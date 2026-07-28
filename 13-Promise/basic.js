fab8e33faa4ab9f2879b2e8ee320952d29bbd6af
Mariam's Github <228037552+mariamrajput78m@users.noreply.github.com>
2026-07-28 20:26:52 +0000

    then & catch in promise

--- 13-Promise/basic.js
+++ 13-Promise/basic.js
@@ -1,27 +1,30 @@
 // Promise Basic Structure
 
-const promiseOne = new Promise ( function()
-{
+const promiseOne = new Promise(function () {
 
 })
 
-const promiseTwo = new Promise ( () => {
+const promiseTwo = new Promise(() => {
 
 })
 
 // this function contains two parameters 
-const promiseONe = new Promise ( function (resolve , reject)
-{
+const promiseONe = new Promise(function (resolve, reject) {
     // Do any Async task here
     // networking , cryptography 
 
-    setTimeout(() =>
-    {
+    setTimeout(() => {
         console.log("Async Task 1 is completed");
         resolve();
-    } , 1000)
+    }, 1000)
 })
 
-promiseONe.then(function (){
-    console.log("Promise is Consumed")
+// then for Success
+promiseONe.then(function () {
+    console.log("Promise is Consumed");
+})
+
+// catch for failure
+promiseONe.catch(function () {
+    console.log("Promise is failed");
 })
