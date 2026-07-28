b67b54e5582405b55fb0a65a5e4fd69773bd7e62
Mariam's Github <228037552+mariamrajput78m@users.noreply.github.com>
2026-07-28 18:01:20 +0000

    selecting elemnts

---
+++ 12-DOM/01-selecting-elements.js
@@ -0,0 +1,29 @@
+// getElementById()
+
+const title = document.getElementById("title");
+
+console.log(title);
+
+// getElementsByClassName()
+
+const paragraphs = document.getElementsByClassName("text");
+
+console.log(paragraphs);
+
+// getElementsByTagName()
+
+const listItems = document.getElementsByTagName("li");
+
+console.log(listItems);
+
+// querySelector()
+
+const firstParagraph = document.querySelector(".text");
+
+console.log(firstParagraph);
+
+// querySelectorAll()
+
+const allParagraphs = document.querySelectorAll(".text");
+
+console.log(allParagraphs);
