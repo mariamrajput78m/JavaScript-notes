5fb2aa295ae8def77dce8bfdd33e1e8daca187a1
Mariam's Github <228037552+mariamrajput78m@users.noreply.github.com>
2026-07-28 18:02:29 +0000

    changing content in js

---
+++ 12-DOM/02-changing-content.js
@@ -0,0 +1,10 @@
+const heading = document.getElementById("title");
+
+// Change Text
+heading.innerText = "Welcome to DOM";
+
+// Change HTML
+heading.innerHTML = "<i>Learning DOM</i>";
+
+// textContent
+console.log(heading.textContent);
