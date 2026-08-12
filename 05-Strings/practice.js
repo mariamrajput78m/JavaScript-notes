9feb742b8d475a6aa546ffcbee19359b63073d16
Mariam's Github <228037552+mariamrajput78m@users.noreply.github.com>
2026-08-12 18:37:25 +0000

    new practice question

--- 05-Strings/practice.js
+++ 05-Strings/practice.js
@@ -1,14 +1,28 @@
 // ============================================================ 
-// // JAVASCRIPT — STRINGS PRACTICE 
-// // ============================================================ 
-// // Focus: 
-// // - Creating strings 
-// // - Concatenation 
-// // - String length 
-// // - Accessing characters 
-// // - String methods 
-// // - Searching strings 
-// // - Extracting strings 
-// // - Template literals 
-// // - Combining strings with other JavaScript concepts 
-// // ============================================================
+ // JAVASCRIPT — STRINGS PRACTICE 
+ // ============================================================ 
+ // Focus: 
+ // - Creating strings 
+ // - Concatenation 
+ // - String length 
+ // - Accessing characters 
+ // - String methods  // - Searching strings 
+ // - Extracting strings 
+ // - Template literals 
+ // - Combining strings with other JavaScript concepts 
+ // ============================================================
+
+// ============================================================ 
+// PART 1 — BASIC STRINGS 
+// ============================================================ 
+// ------------------------------------------------------------ 
+// QUESTION 1 
+// ------------------------------------------------------------ 
+// /* Store: - Your name - Your city - Your favorite food Then print all three. */ 
+// ANSWER: 
+let name = "Mariam"; 
+let city = "Lahore"; 
+let favoriteFood = "Pizza"; 
+console.log(name); 
+console.log(city); 
+console.log(favoriteFood);
