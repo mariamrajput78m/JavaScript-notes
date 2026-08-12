bb1182353239e55aa13ae1407067ac4edcc34e85
Mariam's Github <228037552+mariamrajput78m@users.noreply.github.com>
2026-08-12 18:29:20 +0000

    update

---
+++ 05-Strings/practice.js
@@ -0,0 +1,14 @@
+// ============================================================ 
+// // JAVASCRIPT — STRINGS PRACTICE 
+// // ============================================================ 
+// // Focus: 
+// // - Creating strings 
+// // - Concatenation 
+// // - String length 
+// // - Accessing characters 
+// // - String methods 
+// // - Searching strings 
+// // - Extracting strings 
+// // - Template literals 
+// // - Combining strings with other JavaScript concepts 
+// // ============================================================
