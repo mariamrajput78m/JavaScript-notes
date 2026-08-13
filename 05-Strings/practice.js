d4256f7188d6752cd2e294a9e3c66f2ffbb31c13
Mariam's Github <228037552+mariamrajput78m@users.noreply.github.com>
2026-08-13 16:31:19 +0000

    practice of conversion

--- 05-Strings/practice.js
+++ 05-Strings/practice.js
@@ -26,3 +26,16 @@ let favoriteFood = "Pizza";
 console.log(name); 
 console.log(city); 
 console.log(favoriteFood);
+
+// ------------------------------------------------------------ 
+// QUESTION 2 — STRING CONCATENATION
+// ------------------------------------------------------------ 
+/* Create: firstName = "Mariam" lastName = "Rajput" Join them together and print: Mariam Rajput */
+// ANSWER: 
+let firstName = "Mariam"; 
+let lastName = "Rajput"; 
+let fullName = firstName + " " + lastName; 
+console.log(fullName); 
+// ============================================================ 
+// PART 2 — STRING LENGTH 
+// ============================================================
