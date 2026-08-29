18aac9a542efe2a3b5abba38c96ed5293a1a0060
Mariam's Github <228037552+mariamrajput78m@users.noreply.github.com>
2026-08-29 14:31:38 +0000

    object

---
+++ 09-Objects/practice.js
@@ -0,0 +1,12 @@
+// ============================================
+// PRACTICE: Objects in JavaScript
+// ============================================
+ 
+// ---------- 1. CREATING OBJECTS ----------
+let person = {
+  name: "Mariam",
+  age: 21,
+  isStudent: true,
+};
+ 
+console.log(person);
