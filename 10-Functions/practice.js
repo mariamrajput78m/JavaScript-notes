217a0939e53568775ed7a957506b443cca59c941
Mariam's Github <228037552+mariamrajput78m@users.noreply.github.com>
2026-09-03 18:07:42 +0000

    expressions

--- 10-Functions/practice.js
+++ 10-Functions/practice.js
@@ -14,3 +14,16 @@ function sayHi() {
   return "Hi!";
 }
  
+
+// ---------- 2. FUNCTION EXPRESSION ----------
+// Function stored in a variable - NOT hoisted the same way
+const add = function (a, b) {
+  return a + b;
+};
+console.log(add(3, 4)); // 7
+ 
+// console.log(subtract(5, 2)); // would fail - can't call before this line runs
+const subtract = function (a, b) {
+  return a - b;
+};
+ 
