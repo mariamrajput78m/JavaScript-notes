0f64628dcaad5f493e2d475e163a9f58ec9fd5d4
Mariam's Github <228037552+mariamrajput78m@users.noreply.github.com>
2026-09-09 18:28:18 +0000

    loops in js

---
+++ 11-Loops/practice.js
@@ -0,0 +1,9 @@
+// ============================================
+// PRACTICE: Loops in JavaScript
+// ============================================
+ 
+// ---------- 1. THE CLASSIC FOR LOOP ----------
+// (initialization; condition; increment)
+for (let i = 0; i < 5; i++) {
+  console.log("For loop:", i); // 0, 1, 2, 3, 4
+}
