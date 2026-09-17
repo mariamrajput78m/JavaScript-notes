7e3710877b95d7bef81d320e4157d60b4cf1c043
Mariam's Github <228037552+mariamrajput78m@users.noreply.github.com>
2026-09-17 17:52:23 +0000

    callbacks

---
+++ 13-Callbacks/practice.js
@@ -0,0 +1,6 @@
+// ============================================
+// PRACTICE: DOM (Document Object Model) in JavaScript
+// ============================================
+// NOTE: This file only works when linked inside an actual HTML page
+// (DOM = the browser's live representation of your HTML).
+// Running this file alone in Node.js will NOT work - there's no "document" there.
